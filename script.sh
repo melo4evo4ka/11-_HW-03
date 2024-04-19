8bde9c81865a83cb7b6a59883ae767e8f2b71829
Ubuntu <ubuntu@fhmjk3jeq6otbrb1bu4e.auto.internal>
2024-04-19 14:09:09 +0000

    Change myscript

--- script.sh
+++ script.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-ls -la ${WORKSPACE} 
+#ls -la ${WORKSPACE} 
 localFile = "$(md5sum "${WORKSPACE}/index.html" | cut -d ' ' -f 1)"
 echo "--------"
-echo ${localFile}
+#echo ${localFile}
+file = "$(curl -sL http://158.160.61.221:9889 | md5sum | cut -d ' ' -f 1)"
+
+if [ "$localFile" = "$file" ]; then
+	echo "success"
+else
+	echo "Failed md5"
+fi
+
