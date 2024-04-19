1959979da133c372e6df27c52f4959279296e516
Ubuntu <ubuntu@fhmjk3jeq6otbrb1bu4e.auto.internal>
2024-04-19 15:01:52 +0000

    change script

--- script.sh
+++ script.sh
@@ -6,6 +6,9 @@ echo "--------"
 #echo ${localFile}
 file = "$(curl -sL http://158.160.61.221:9889 | md5sum | cut -d ' ' -f 1)"
 
+echo $localFile
+echo $file
+
 if [ localFile == file ]; then
 	echo "success"
 else
