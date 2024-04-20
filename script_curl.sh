5e714b1888f09ac28808a9bab449ced2ffcbfb4b
Ubuntu <ubuntu@fhmjk3jeq6otbrb1bu4e.auto.internal>
2024-04-20 12:38:52 +0000

    change script_curl.sh

--- script_curl.sh
+++ script_curl.sh
@@ -4,3 +4,4 @@ response=$(curl --write-out '%{http_code}' --silent --output /dev/null 'http://1
 echo $response
 curl -s -X POST https://api.telegram.org/bot7126049267:AAGbxN5AHRzFgXa_ENMC8xdAVjJCQuy2lSI/sendMessage -d chat_id=729788143 -d parse_mode=markdown -d text="*STATUS CODE* : $response"
 
+
